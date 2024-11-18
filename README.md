96626112f9d5498bb7eae147d95c631e49c820f2
SpiffGreen <spiffjekeygreen@gmail.com>
2024-11-18 21:21:35 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Mydict
+
+A simple cli dictionary app, simply ask `mydict <insert_word>` and it finds it.
+In the case it doesn't it give similar words.
+
+## Credits
+The word dictionary used in this projects was gotten from [Ajaybhatia's Gist](https://gist.github.com/ajaybhatia/12c43431aeb08b118bb3f2e7dcbbc501#file-english-dictionary-json).
+
+## Author
+Spiff Jekey-Green <spiffjekeygreen@gmail.com>
+
+## Licence
+[MIT](./LICENSE)
---
+++ src/main.rs
@@ -0,0 +1,75 @@
+use clap::Parser;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::Read;
+use std::process;
+use strsim::levenshtein;
+use colored::*;
+
+/// A simple cli dictionary app, simply ask `mydict <insert_word>` and it finds it.
+/// In the case it doesn't it give similar words.
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Word to search
+    #[arg(index = 1)]
+    word: String,
+
+    /// Show/remove similar words from results
+    #[arg(short, long, default_value_t = String::from("true"))]
+    show_suggestion: String,
+}
+
+fn load_dictionary(file_path: &str) -> HashMap<String, Vec<String>> {
+    let mut file = File::open(file_path).unwrap();
+    let mut data = String::new();
+    file.read_to_string(&mut data).unwrap();
+
+    let json: HashMap<String, Vec<String>> = serde_json::from_str(&data).unwrap();
+    json
+}
+
+fn find_closest_match(word: &str, dictionary: HashMap<String, Vec<String>>) -> Option<String> {
+    dictionary
+        .keys()
+        .min_by_key(|key| levenshtein(word, key))
+        .cloned()
+}
+
+const DICTIONARY_PATH: &str = "/Users/spiffgreen/work/mydict/src/english-dictionary.json";
+
+fn main() {
+    let args: Args = Args::parse();
+    let dictionary: HashMap<String, Vec<String>> = load_dictionary(&DICTIONARY_PATH);
+    let mut similar_word: Option<std::string::String> = None;
+    let mut show_suggestion = true;
+
+    match args.show_suggestion.as_str() {
+        "true" => similar_word = find_closest_match(&args.word, dictionary.clone()),
+        "false" => show_suggestion = false,
+        _ => {
+            eprintln!("Invalid value for show_similar flag");
+            process::exit(1)
+        }
+    }
+
+    println!("Meaning of {}:", args.word);
+
+    if dictionary.contains_key(&args.word) {
+        for (idx, definitions) in dictionary.get(&args.word).unwrap().iter().enumerate() {
+            println!("{}. {}", idx + 1, definitions);
+        }
+    } else {
+        if show_suggestion {
+            if let Some(suggestion) = similar_word {
+                println!("Did you mean, {}?", suggestion);
+            } else {
+                println!("No Similar words.")
+            }
+        }
+        let error_message = format!("Word {} is not in dictionary.",  args.word).red();
+        eprintln!("{}", error_message);
+        process::exit(1)
+    }
+}
